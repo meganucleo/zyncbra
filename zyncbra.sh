ad0541942cf5cdf9a995b46292b6e9f0fa995733
León Ramos <leon.ramos@meganucleo.mx>
2021-05-17 22:38:23 -0500

    translating to english

--- zyncbra.sh
+++ zyncbra.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#Script para sincronizar buzones de 2 zimbras en delti
+#Script for syncing to zimbra servers
 #author Leon Ramos 2021 @fulvous
 
-if [ -z ${1} ] ; then
+HOST1="192.168.1.1"
+HOST2="192.168.1.2"
+
+if [ -z ${2} ] ; then
         echo "USAGE: ${0} textfile.csv"
         echo -e "\ttextfile.csv contains 'user,password', one line per mailbox"
         echo
@@ -21,7 +24,7 @@ function sync {
         { while IFS=',' read user password fake
         do
                 echo "===================Sincronizando ${user}========================"
-                imapsync --host1 10.1.3.66 --user1 ${user} --password1 ${password} --authmech1 PLAIN --ssl1 --host2 10.1.3.4 --user2 ${user} --password2 ${password} --authmech2 PLAIN --ssl2
+                imapsync --host1 ${HOST1} --user1 ${user} --password1 ${password} --authmech1 PLAIN --ssl1 --host2 ${HOST2} --user2 ${user} --password2 ${password} --authmech2 PLAIN --ssl2
                 if [ $? -eq 0 ] ; then
                         logger "imapsync ${user} CORRECTO"
                 else
